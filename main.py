5aa4032b0fd20efadaa4af6a152585480180ab2d
raman-bhatia <i.raman.bhatia@gmail.com>
2023-05-01 07:32:27 -0700

    widgets

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import tkinter as tk
+
+# def hello(event):
+    # print("Hello World!")
+
 window = tk.Tk()
 window.title("My first GUI App")
 window.geometry("1000x600")
 
-mylabel = tk.Label(text = "Hello World")
-mylabel.grid(column=0, row=0)
+myEntry = tk.Entry(window)
+myEntry.grid(column=0, row=0)
+
+# mylabel = tk.Label(text = "Hello World")
+# mylabel.grid(column=0, row=0)
+# mylabel.pack(side=tk.TOP)
+
+# mylabel2 = tk.Label(text = "How are you?")
+# mylabel2.grid(column=1, row=1)
+
+# mybutton = tk.Button(window, text = "Click Me")
+# mybutton.grid(column=0, row=1)
+# mybutton.bind("<Button-1>", hello)
 
 window.mainloop()
